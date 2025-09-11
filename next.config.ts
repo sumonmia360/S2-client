036077fc06df360421e89402c75736280e66779a
sumonmia360 <sumonmia4526@gmail.com>
2025-09-11 18:33:48 +0600

    Alhamdulillah picsum remove it done

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-      },
-    ],
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "picsum.photos",
+    //   },
+    // ],
   },
   devIndicators: false,
   eslint: {
