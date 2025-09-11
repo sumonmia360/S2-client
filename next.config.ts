015d2e4cfce593f4458088c5db0feadc3a0eb0fc
sumonmia360 <sumonmia4526@gmail.com>
2025-09-11 20:15:34 +0600

    he inke uni

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos", // add other hostnames if needed
+      },
+    ],
+    unoptimized: true, // disables server-side image optimization
+  },
   devIndicators: false,
   eslint: {
     ignoreDuringBuilds: true,
